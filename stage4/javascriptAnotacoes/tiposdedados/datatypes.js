9d6dfb3af1fd70d3a3b5e5c79c2008d5ab0689e9
Davi Thomeny Cotting <122418493+davicotting@users.noreply.github.com>
2023-09-26 09:47:33 -0300

    data types

---
+++ stage4/javascriptAnotacoes/tiposdedados/datatypes.js
@@ -0,0 +1,60 @@
+
+    //  Tipos de Dados
+
+    // String - Cadeia de caracteres.
+
+    let name = "Davi"; 
+
+    // Number - Qualquer tipo de valor como: numeros inteiros, float, double etc...
+
+    let number = 18.000;
+
+    console.log(typeof number);
+
+    // Boolean - Tipo que retorna apenas dois valores: true ou false.
+
+    if (number = 18.000){
+        number = true;
+        console.log(number);
+    }
+
+    // Undefined - a variavel nao esta definida.
+
+    let itsUndefined = undefined;
+
+    console.log(itsUndefined); 
+
+    // Null - a variavel existe, porem não esta preenchida com algum valor.
+
+    let x = null;
+
+    console.log("teste: ", x === null);
+
+    // Objeto - é um tipo independente de peopriedades e tipos.
+    
+    let object = ({
+        name: "Davizera",
+        age: 18,
+        gender: "masculino",
+        playBasketball: true
+    });
+
+    // possivel tambem acessar valores es propridades especificas daquele objeto.
+
+    console.log(object);
+
+    console.log(object.name);
+
+    console.log(object.playBasketball);
+
+   // Array -  Uma lista que pode ter tamanho definido e propriedades com tipagens diferentes
+
+   let array = [1, "stringzinha", true];
+
+   console.log(array);
+
+
+
+    
+
+    
